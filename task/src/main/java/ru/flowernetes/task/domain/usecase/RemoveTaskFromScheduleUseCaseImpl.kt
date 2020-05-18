948c46db6a92b619a66f6a439f3d58b7dbdbbcf2
b1nd <h33el@yandex.ru>
2020-05-18 16:53:27 +0300

    unmark dependencies on removing task from schedule

--- task/src/main/java/ru/flowernetes/task/domain/usecase/RemoveTaskFromScheduleUseCaseImpl.kt
+++ task/src/main/java/ru/flowernetes/task/domain/usecase/RemoveTaskFromScheduleUseCaseImpl.kt
@@ -6,12 +6,14 @@ import ru.flowernetes.monitoring.api.domain.usecase.GetTaskStatusInfoUseCase
 import ru.flowernetes.monitoring.api.domain.usecase.SendWorkflowTaskStatusMessageUseCase
 import ru.flowernetes.scheduling.api.domain.usecase.RemoveTaskScheduleUseCase
 import ru.flowernetes.task.api.domain.usecase.RemoveTaskFromScheduleUseCase
+import ru.flowernetes.task.api.domain.usecase.UnmarkTaskDependencyMarkersUseCase
 import ru.flowernetes.task.data.repo.TaskRepository
 
 @Component
 class RemoveTaskFromScheduleUseCaseImpl(
   private val removeTaskScheduleUseCase: RemoveTaskScheduleUseCase,
   private val getTaskStatusInfoUseCase: GetTaskStatusInfoUseCase,
+  private val unmarkTaskDependencyMarkersUseCase: UnmarkTaskDependencyMarkersUseCase,
   private val sendWorkflowTaskStatusMessageUseCase: SendWorkflowTaskStatusMessageUseCase,
   private val taskRepository: TaskRepository
 ) : RemoveTaskFromScheduleUseCase {
@@ -20,6 +22,7 @@ class RemoveTaskFromScheduleUseCaseImpl(
         taskRepository.save(task.copy(scheduled = false)).also {
             removeTaskScheduleUseCase.exec(task)
         }
+        unmarkTaskDependencyMarkersUseCase.exec(task)
         val taskStatusInfo = getTaskStatusInfoUseCase.exec(task)
         sendWorkflowTaskStatusMessageUseCase.exec(task.workflow, taskStatusInfo)
     }
